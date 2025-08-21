293378c432c06f39f295e2e284684c7927703f75
Kush Gupta <Kushg827@gmail.com>
2025-08-21 16:37:13 +0530

    setup node mailer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
-
+const {sendBasicEmail} = require('./services/email-service.js')
 const app = express();
 
 const setupAndStartServer = () => {
@@ -9,6 +9,13 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
+
+        // sendBasicEmail(
+        //     'mailfrom@gmail.com',
+        //     'kushg5849@gmail.com',
+        //     'This is a test email',
+        //     "Hey how are you ?"
+        // )
     });
 }
 
