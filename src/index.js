cef2dfc2b85dcae6fb982ac3525ed8f37d5bbecd
Kush Gupta <Kushg827@gmail.com>
2025-08-22 16:08:15 +0530

    Added setup for message queue

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const { PORT } = require('./config/serverConfig');
 const {sendBasicEmail} = require('./services/email-service.js');
 const TicketController = require("./controllers/ticket-controller");
 const {setupJobs} = require('./utils/job.js');
+const {createChannel} = require('./utils/messageQueue.js');
+
 const app = express();
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
 
 
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
+
+    const channel = await createChannel();
 
     app.post('/api/v1/tickets', TicketController.create);
 
